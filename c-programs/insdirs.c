1b9b66ecd528d1dcd2804aab4d2a813d090164b7
Hans <hanlof@gmail.com>
2016-11-05 12:26:50 +0100

    WIP. maybe broken

--- c-programs/insdirs.c
+++ c-programs/insdirs.c
@@ -4,9 +4,6 @@
 
 char * compare_path(char * p, char * c)
 {
-  p -= 2;
-  c -= 2;
-
   while (*p != '/' && *p != '\n')
     p--;
 
@@ -26,26 +23,26 @@ char * compare_path(char * p, char * c)
 }
 
 #define ALLOCSIZE 0x800000
+#define READSIZE 0x10
 
 int main(int argc, int * argv[]) {
   char * buf_start = malloc(ALLOCSIZE);
 
-  // put newline as a mark at the beginning of the buffer. it will simplify the compare function.
-  buf_start[0] = buf_start[1] = '\n';
-  buf_start += 2;
+  *buf_start++ = '\n'; // put newline as a mark at the beginning of the buffer. it will simplify the compare function.
+
   char * cur_line_start = buf_start;
   char * cur_line_end = buf_start;
   int i;
-  i = read(0, buf_start, 4096);
+  i = read(0, buf_start, READSIZE);
   char * buf_end = buf_start + i;
   while (1) {
     if (i == 0) {
       break;
     }
     while (cur_line_end < buf_end) {
-      while ('\n' != *cur_line_end++) {
+      while ('\n' != *++cur_line_end) {
         if (cur_line_end == buf_end) {
-          i = read(0, buf_end, 4096);
+          i = read(0, buf_end, READSIZE);
           if (i == 0) {
             exit(0);
           }
@@ -53,16 +50,14 @@ int main(int argc, int * argv[]) {
         }
       }
       // cur_line_start is pretty much the same as previous line end
-      char * r = compare_path(cur_line_start, cur_line_end); // r becomes a pointer to the last slash
+      char * r = compare_path(cur_line_start - 1, cur_line_end - 1); // r becomes a pointer to the last slash
       if (r > cur_line_start) {
-        char lf = '\n';
         write(1, cur_line_start, (int)(r - cur_line_start)+1);
-        write(1, &lf, 1);
       }
       write(1, cur_line_start, (cur_line_end - cur_line_start));
       cur_line_start = cur_line_end;
     }
-    i = read(0, buf_end, 4096);
+    i = read(0, buf_end, READSIZE);
     buf_end += i;
   }
 }
