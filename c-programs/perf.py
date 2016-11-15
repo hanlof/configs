be178522cb5587dbe8c51ee9f3d3acb1a388f56f
Hans <hanlof@gmail.com>
2016-11-15 21:02:34 +0100

    handle ctrl-c in perf.py

--- c-programs/perf.py
+++ c-programs/perf.py
@@ -4,18 +4,23 @@ import sys
 import re
 import subprocess
 
-tot_systime = 0.0
-tot_usertime = 0.0
-samples = 0
-while True:
-  (_, out) = subprocess.Popen(["bash", "-c", "TIMEFORMAT=S%3S\" \"U%3U; time ./insdirs2 < all-files > /dev/null"], stderr=subprocess.PIPE).communicate()
-  m=re.match("S([0123456789.]+) U([0123456789.]+)", out)
-  systime = float(m.group(1))
-  usertime = float(m.group(2))
-  samples = samples + 1
-  tot_systime = tot_systime + systime
-  tot_usertime = tot_usertime + usertime
-  
-  print "avg %.3f %.3f" % (tot_systime / samples, tot_usertime / samples)
-  up = "%c[A" % 27
-  print up,
+try:
+  tot_systime = 0.0
+  tot_usertime = 0.0
+  samples = 0
+  while True:
+    (_, out) = subprocess.Popen(["bash", "-c", "TIMEFORMAT=S%3S\" \"U%3U; time ./insdirs2 < all-files > /dev/null"], stderr=subprocess.PIPE).communicate()
+    
+    m=re.match("S([0123456789.]+) U([0123456789.]+)", out)
+    systime = float(m.group(1))
+    usertime = float(m.group(2))
+    samples = samples + 1
+    tot_systime = tot_systime + systime
+    tot_usertime = tot_usertime + usertime
+    
+    print "avg %.3f %.3f" % (tot_systime / samples, tot_usertime / samples)
+    up = "%c[A" % 27
+    print up,
+except KeyboardInterrupt as k:
+  print "Stopped after %i iterations" % samples
+  sys.exit(1)
