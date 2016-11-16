8d709a67236edebe7c80a7040847e2b4de4b28fd
Hans <hanlof@gmail.com>
2016-11-16 23:58:57 +0100

    perf.py support for total time reporting

--- c-programs/perf.py
+++ c-programs/perf.py
@@ -9,17 +9,20 @@ try:
   up = "%c[A" % 27
   tot_systime = 0.0
   tot_usertime = 0.0
+  tot_realtime = 0.0
   samples = 0
   while True:
-    (_, out) = subprocess.Popen(["bash", "-c", "TIMEFORMAT=S%3S\" \"U%3U; time ./insdirs2 < all-files > /dev/null"], stderr=subprocess.PIPE).communicate()
+    (_, out) = subprocess.Popen(["bash", "-c", "TIMEFORMAT=S%3S\" U%3U \"R%3R; time ./insdirs2 < all-files > /dev/null"], stderr=subprocess.PIPE).communicate()
     
-    m=re.match("S([0123456789.]+) U([0123456789.]+)", out)
+    m=re.match("S([0123456789.]+) U([0123456789.]+) R([0123456789.]+)", out)
     systime = float(m.group(1))
     usertime = float(m.group(2))
+    realtime = float(m.group(3))
     samples = samples + 1
     tot_systime = tot_systime + systime
     tot_usertime = tot_usertime + usertime
-    print up + "avg %.3f %.3f" % (tot_systime / samples, tot_usertime / samples)
+    tot_realtime = tot_realtime + realtime
+    print up + "avg %.3f %.3f %.3f" % (tot_systime / samples, tot_usertime / samples, tot_realtime / samples)
 except KeyboardInterrupt as k:
   print "Stopped after %i iterations" % samples
   sys.exit(1)
