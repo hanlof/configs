afbecf4431300265d609e1a6508c3f814681d0ac
Hans Löfvig <hans.lofving@autoliv.com>
2016-11-29 17:09:35 +0100

    insdirs2: implement buffer cycling

--- c-programs/insdirs2.c
+++ c-programs/insdirs2.c
@@ -92,25 +92,35 @@ int main(int argc, char * argv[]) {
 	parse_args(argc, argv, &params);
 
 	char * alloc_ptr = malloc(params.allocsize);
+	char * cycle_marker = alloc_ptr + params.allocsize - params.readsize; // marks where to wrap around (cycle) the buffer
 	char * read_ptr = alloc_ptr; // marks where next read should happen (we have not read data past this point)
 	*read_ptr++ = '\n'; // put EOL marker at start of buffer to simplify compare_path() implementation
 	char * write_ptr = read_ptr; // marks what we have written to stdout so far/what we should write next
 	char * start_of_line = read_ptr; // marks the start of last line of input
 	char * last_slash = read_ptr;    // marks the last '/' found so far
 	char * prev_last_slash = read_ptr; // marks the last slash in previous line
-	char * ptr = read_ptr; // current position in the buffer
-	char * cycle_marker = alloc_ptr + params.allocsize - params.readsize; // marks where to wrap around (cycle) the buffer
 	int i; // temp integer for syscall return values
 	char tmp; // temp storage for when we temporarily insert '\n' into buffer and need to save original character
+	char * ptr = read_ptr; // current position in the buffer
 
 	while (1) {
 		if (ptr == read_ptr) {
 			if (read_ptr >= cycle_marker) {
 				// first output to stdout so that write_ptr is not too far behind
+                                i = write(STDOUT_FILENO, write_ptr, start_of_line - write_ptr);
+                                write_ptr = start_of_line;
 				// then copy stuff to beginning of buffer and update pointers
+                                memcpy(alloc_ptr, read_ptr - 2 * PATH_MAX, 2 * PATH_MAX);
+                                read_ptr = alloc_ptr + 2 * PATH_MAX;
+                                int diff = ptr - read_ptr;
+                                write_ptr -= diff;
+                                start_of_line -= diff;
+                                last_slash -= diff;
+                                prev_last_slash -= diff;
+                                ptr -= diff;
 				//  ! prev_last_slash does need to be copied. this means pathmax times two need to be copied
 
-				fprintf(stderr, "%p %p\n", ptr, alloc_ptr + params.allocsize);
+			//	fprintf(stderr, "%p %p\n", ptr, alloc_ptr + params.allocsize);
 			}
 			i = read(STDIN_FILENO, read_ptr, params.readsize);
 			// XXX take care of negative return value
@@ -119,7 +129,6 @@ int main(int argc, char * argv[]) {
 			}
 			read_ptr += i;
 		}
-
 		if (*ptr == '\n') {
 			if (compare_path(prev_last_slash, last_slash)) {
 				tmp = last_slash[1]; // last_slash[1] is always inside the allocated space because we cycle way before the end
@@ -136,8 +145,6 @@ int main(int argc, char * argv[]) {
 		}
 		ptr++;
 	}
-
 	i = write(STDOUT_FILENO, write_ptr, (int)(ptr - write_ptr));
-
 	return 0;
 }
