d7b0ec0328a1e04e8da5ba41cae5d4e2653bd8bb
Hans Löfving <hans.lofving@autoliv.com>
2016-11-30 14:16:08 +0100

    insdirs2: fix bug and refactor variable name

--- c-programs/insdirs2.c
+++ c-programs/insdirs2.c
@@ -8,12 +8,12 @@
 // do sanity check on input params
 // - alloc size > PATH_MAX
 // - alloc size > 4 * read_size or something like that
-// maybe use mmap instead of malloc/read?
-// - investigate performance
-// - also investigate memory consumtion for mmap or huge file
-// - maybe its possible to rememmap during execution
 // experiment: compare buffer cycling to just reallocing
 // experiment: compare this approach (allocate several megabytes) with a really small buffer and constantly wrapping pointers
+// interesting: frequent read():ing and buffer cycling does not seem to impact performance much when readsize >= pagesize
+//   maybe we can get away with using really little memory
+//   also why do we read at readsize bytes before the end of buffer? it means we never use the last readsize bytes in the buffer
+//   we do add a newline character at one byte ahead of ptr so we need to be careful to always make sure ptr stays one byte away from the end of the buffer or change the implementation of outputting newline? maybe its already safe
 
 int compare_path(char * p, char * c)
 {
@@ -73,7 +73,6 @@ void parse_args(int argc, char * argv[], struct params * par)
 			// We dont need to print the bad option. getopt() does it automatically
 			exit(1); // TODO: print help
 		}
-
 		if (NULL != whatopt) {
 			i = strtol(optarg, &t, 0);
 			if ('\0' != *t) {
@@ -85,6 +84,20 @@ void parse_args(int argc, char * argv[], struct params * par)
 	}
 }
 
+void check_args(struct params * par)
+{
+	// readsize and allocsize are only used to measure performance and tune those values.
+	// they will not be very useful in practice.
+	// we will stay on the safe side with the checking. dont make it too complicated
+	// alloc size must be at least 4 PATH_MAX plus read size
+	// why? we copy 2 * PATH_MAX using memcpy and memcpy cant have overlapping regions
+	if (par->allocsize < 4 * PATH_MAX + par->readsize) {
+		printf("allocsize must be four times PATH_MAX(%d) plus readsize bytes. (%d bytes in total)\n", PATH_MAX, 4 * PATH_MAX + par->readsize);
+		printf("readsize:%d allocsize:%d\n", par->readsize, par->allocsize);
+		exit(1);
+	}
+}
+
 #define MAX(a, b) ( (a > b) ? (a) : (b) )
 int main(int argc, char * argv[]) {
 	struct params params = {
@@ -92,7 +105,7 @@ int main(int argc, char * argv[]) {
 		.allocsize = 8 * MAX(PATH_MAX, getpagesize()),
 	};
 	parse_args(argc, argv, &params);
-	//check_args(&params);
+	check_args(&params);
 
 	char * alloc_ptr = malloc(params.allocsize);
 	char * cycle_marker = alloc_ptr + params.allocsize - params.readsize; // marks where to wrap around (cycle) the buffer
@@ -102,42 +115,43 @@ int main(int argc, char * argv[]) {
 	char * start_of_line = read_ptr; // marks the start of last line of input
 	char * last_slash = read_ptr;	 // marks the last '/' found so far
 	char * prev_last_slash = read_ptr; // marks the last slash in previous line
-	int i; // temp integer for syscall return values
-	char tmp; // temp storage for when we temporarily insert '\n' into buffer and need to save original character
+	int tmp; // temp integer for syscall return values and other stuff
+	char c; // temp storage for when we temporarily insert '\n' into buffer and need to save original character
 	char * ptr = read_ptr; // current position in the buffer
 
 	while (1) {
 		if (ptr == read_ptr) {
 			if (read_ptr >= cycle_marker) {
 				// first output to stdout so that write_ptr is not too far behind
-				i = write(STDOUT_FILENO, write_ptr, start_of_line - write_ptr);
+				tmp = write(STDOUT_FILENO, write_ptr, start_of_line - write_ptr);
 				write_ptr = start_of_line;
 				// copy stuff to beginning of buffer. why 2 * PATH_MAX?
 				// because last_slash may be PATH_MAX behind ptr
 				// and prev_last_slash may be PATH_MAX behind that
 				memcpy(alloc_ptr, read_ptr - 2 * PATH_MAX, 2 * PATH_MAX);
 				// fixup all the pointers
-				int diff = ptr - read_ptr;
-				write_ptr -= diff;
-				start_of_line -= diff;
-				last_slash -= diff;
-				prev_last_slash -= diff;
-				ptr -= diff;
 				read_ptr = alloc_ptr + 2 * PATH_MAX;
+				tmp = ptr - read_ptr;
+				write_ptr -= tmp;
+				start_of_line -= tmp;
+				last_slash -= tmp;
+				prev_last_slash -= tmp;
+				ptr -= tmp;
+				printf("read_ptr:%p ptr:%p\n", read_ptr, ptr);
 			}
-			i = read(STDIN_FILENO, read_ptr, params.readsize);
+			tmp = read(STDIN_FILENO, read_ptr, params.readsize);
 			// XXX take care of negative return value
-			if (0 == i) {
+			if (0 == tmp) {
 				break;
 			}
-			read_ptr += i;
+			read_ptr += tmp;
 		}
 		if (*ptr == '\n') {
 			if (compare_path(prev_last_slash, last_slash)) {
-				tmp = last_slash[1]; // last_slash[1] is always inside the allocated space because we cycle way before the end
+				c = last_slash[1]; // last_slash[1] is always inside the allocated space because we cycle way before the end
 				last_slash[1] = '\n';
-				i = write(STDOUT_FILENO, write_ptr, (int)(last_slash - write_ptr + 2));
-				last_slash[1] = tmp;
+				tmp = write(STDOUT_FILENO, write_ptr, (int)(last_slash - write_ptr + 2));
+				last_slash[1] = c;
 				// FIXME: handle write() returning less than wanted number of bytes
 				write_ptr = start_of_line;
 			}
@@ -148,6 +162,6 @@ int main(int argc, char * argv[]) {
 		}
 		ptr++;
 	}
-	i = write(STDOUT_FILENO, write_ptr, (int)(ptr - write_ptr));
+	tmp = write(STDOUT_FILENO, write_ptr, (int)(ptr - write_ptr));
 	return 0;
 }
