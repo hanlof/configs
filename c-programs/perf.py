9727b17c2d8f0d865e5b92143db3d931497cd62b
Hans Löfving <hans.lofving@autoliv.com>
2016-12-01 14:15:51 +0100

    add argparse to perf.py
    get infile and outfile from command line

--- c-programs/perf.py
+++ c-programs/perf.py
@@ -6,21 +6,26 @@ import subprocess
 import resource
 import os
 import time
-
+import argparse
 # DONE eliminate os.system() it spaws an extra /bin/sh and we want to avoid that. 
 # DONE measure real time for total time instead of adding user time and system time
+# DONE add parameter for infile/outfile
 # TODO add parameter for quitting after N iterations or after M seconds or after stabilized value
-# TODO add parameter for infile/outfile
 # TODO print other statistics from the stat structure
+#  --- handle all the different performance measurements in a generic way? make it easier to add measuring points
 # TODO store stuff in database. or make a wrapper that does that??
 #  --- will need to think about parameters into the executed program and how to store them properly in tables or whatever
-# TODO handle all the different performance measurements in a generic way. make it easier to add measuring points
-if len(sys.argv) < 2:
-    print("what do you want to run?")
-    sys.exit(0)
 
-command = " ".join(sys.argv[1:])
-old_mtime = os.stat(sys.argv[1]).st_mtime
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', dest='infile', type=argparse.FileType('rb'), default=sys.stdin)
+parser.add_argument('-o', dest='outfile', type=argparse.FileType('wb'), default=sys.stdout)
+parser.add_argument('program')
+parser.add_argument('program_args', nargs=argparse.REMAINDER)
+
+args = parser.parse_args(sys.argv[1:])
+print args
+
+old_mtime = os.stat(args.program).st_mtime
 
 def wait_for_file(f):
     back = "%c[5D" % 27
@@ -38,7 +43,7 @@ def wait_for_file(f):
     mtime = None
     while mtime is None:
         try:
-            stat=os.stat(sys.argv[1])
+            stat=os.stat(args.program)
             mtime=stat.st_mtime
         except OSError as e:
             time.sleep(0.1)
@@ -51,7 +56,6 @@ def wait_for_file(f):
     print " Got it!"
     return mtime
 
-FNULL = open("/dev/null", 'wb')
 try:
   print ""
   up = "%c[A" % 27
@@ -63,24 +67,24 @@ try:
   while True:
     res_before=resource.getrusage(resource.RUSAGE_CHILDREN)
     try:
-        mtime=os.stat(sys.argv[1]).st_mtime
+        mtime=os.stat(args.program).st_mtime
     except OSError as e:
-        mtime=wait_for_file(sys.argv[1])
+        mtime=wait_for_file(args.program)
     if mtime != old_mtime:
-        print sys.argv[1] + " changed!\n"
+        print args.program + " changed!\n"
         old_mtime = mtime
         tot_systime = 0.0
         tot_usertime = 0.0
         tot_realtime = 0.0
         tot_cputime = 0.0
         samples = 0
-    #i=os.system(command)
-    FIN = open("./all-files", 'rwb')
+
+    args.infile.seek(0)
     before=time.time()
-    p=subprocess.Popen(sys.argv[1:], stdin=FIN, stdout=FNULL)
+    p=subprocess.Popen(args.program, stdin=args.infile, stdout=args.outfile)
     i=p.wait()
     after=time.time()
-    FIN.close()
+
     status, sig = (i >> 8, i & 255)
     status = 0
     sig = 0
