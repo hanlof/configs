37073b2b743e3d1edb7f39348964abcdcbebfd83
Hans Löfving <hans.lofving@volvocars.com>
2017-05-21 23:07:01 +0200

    Placeholder file for configs installer (including notes)
    Also updated .gitignore for vim helptags

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Placeholder file with some notes...
+
+# TODO: fetch subrepos (skip dmenu if it can be found or installed globally (using apt-get)
+# TODO: compile helpers (again skip dmenu if its not needed)
+# TODO: modify template files so "configs"-path can easily be sed:ed into them
+# TODO: update template files so they can use globally installed dmenu if it exists
+# TODO: find path to this git repo and sed real path into template files
+# TODO: add sed:ed template files to users real dotfiles. gonna need some interactivity if files exists
+# TODO: consider adding index_repo to path and/or as an alias in configs/bashrc
+# TODO: run :helptags ALL in vim once
