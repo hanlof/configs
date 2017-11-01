33681d6ff19680034587b0ce57aab4ad39f4dbae
Hans Löfving <hans.lofving@volvocars.com>
2017-11-01 14:26:11 +0100

    hacky beginning of install.sh
    also some more TODO stuff in install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,20 @@
 
 # TODO: fetch subrepos (skip dmenu if it can be found or installed globally (using apt-get)
 # TODO: compile helpers (again skip dmenu if its not needed)
+# TODO: consider dmenu versions. some versions do not recognize the -w flag
+# TODO: consider dmenu dependencies: libx11-dev libxinerama-dev libxft-dev
 # TODO: modify template files so "configs"-path can easily be sed:ed into them
 # TODO: update template files so they can use globally installed dmenu if it exists
 # TODO: find path to this git repo and sed real path into template files
 # TODO: add sed:ed template files to users real dotfiles. gonna need some interactivity if files exists
 # TODO: consider adding index_repo to path and/or as an alias in configs/bashrc
-# TODO: run :helptags ALL in vim once
+# TODO: run :helptags ALL in vim once (ignore error about system-wide write permission)
+# TODO: sudo apt-get install exuberant-ctags vim-gtk gitk. what else??
+
+git submodule update --init
+(cd c-programs
+make insdirs2
+make prefix)
+(cd submodules/dmenu
+sudo apt-get install libx11-dev libxinerama-dev libxft-dev
+make)
