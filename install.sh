302966aab310fc37af1a18cd38328cf308d6900b
Hans Löfving <hans.loving@consultant.volvo.com>
2018-04-06 10:51:27 +0200

    add vim helptags and update todo items

--- install.sh
+++ install.sh
@@ -4,27 +4,16 @@
 
 # DONE: fetch subrepos (skip dmenu if it can be found or installed globally (using apt-get)
 # DONE: compile helpers (again skip dmenu if its not needed)
+# DONE: run :helptags for vim-fugitive
 
 # TODO: consider global git options (hooks) that may interfere with this local git repo and/or other repos
-# TODO: consider dmenu versions. some versions do not recognize the -w flag
-# TODO: modify template files so "configs"-path can easily be sed:ed into them
+# TODO: consider dmenu versions in vim and bash. some versions do not recognize the -w flag
 # TODO: update vim config so it can detect dmenu path
-# TODO: find path to this git repo and sed real path into template files
-# TODO: add sed:ed template files to users real dotfiles. gonna need some interactivity if files exists
+# TODO: do some SED magic for paths and install template files. gonna need some interactivity if files exists
 # TODO: consider adding index_repo to path and/or as an alias in configs/bashrc
-# TODO: run :helptags ALL in vim once (ignore error about system-wide write permission)
 # TODO: sudo apt-get install exuberant-ctags vim-gtk gitk. what else??
 # TODO: check repo root hash in vimrc_template. set up indentation style to comply with repo coding standards
 # TODO: consider different indentation styles in different paths within a repo?
-
-# alternative1: screw system-wide dmenu. always compile. why not? if compilation fails for whatever reason we need to fall back to system-wide anyway.
-# alternative2: check system-wide dmenu version after install. compile if it not > 4.6
-
-# install system-wide dmenu
-# check version of system-wide dmenu. if it has -w then we are fine!
-# if we cannot install dmenu or if we dont have -w we compile
-# if system-wide install and/or compilation fails then we are screwed
-# vim and bash need to check dmen version either way
 
 CONFIGS_PATH=$(dirname $0)
 
@@ -45,6 +34,7 @@ which dmenu > /dev/null || test -x ${CONFIGS_PATH}/submodules/dmenu/dmenu || {
 
 # fugitive vim plugin
 git -C ${CONFIGS_PATH} submodule update --init submodules/vim-fugitive
+vim -n -e --noplugin --cmd  "helptags submodules/vim-fugitive|quit"
 
 # c-program helpers
 (
