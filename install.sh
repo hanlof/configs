ca7837f6d7cfc08a6212e39eeb8b9ca5b39e147d
Hans Lofving <hanlof@gmail.com>
2018-04-06 22:16:06 +0100

    git configuration added to install.sh, gitconfig_templade removed because git config mechanism is used instead!

--- install.sh
+++ install.sh
@@ -5,15 +5,18 @@
 # DONE: fetch subrepos (skip dmenu if it can be found or installed globally (using apt-get)
 # DONE: compile helpers (again skip dmenu if its not needed)
 # DONE: run :helptags for vim-fugitive
+# DONE: install gitconfig include using git config mechanism
 
 # TODO: consider global git options (hooks) that may interfere with this local git repo and/or other repos
-# TODO: consider dmenu versions in vim and bash. some versions do not recognize the -w flag
-# TODO: update vim config so it can detect dmenu path
-# TODO: do some SED magic for paths and install template files. gonna need some interactivity if files exists
+# TODO: update vim config so it can detect dmenu path, and only use -w if version is >= 4.7
 # TODO: consider adding index_repo to path and/or as an alias in configs/bashrc
 # TODO: sudo apt-get install exuberant-ctags vim-gtk gitk. what else??
 # TODO: check repo root hash in vimrc_template. set up indentation style to comply with repo coding standards
 # TODO: consider different indentation styles in different paths within a repo?
+# TODO: do some SED magic for paths and install template files. gonna need some interactivity if files exists
+# TODO: maybe set up core.email and core.name for git configuration
+# TODO: maybe set up github username for configs repo in git configuration (local or global?)
+
 
 CONFIGS_PATH=$(dirname $0)
 
@@ -33,12 +36,26 @@ which dmenu > /dev/null || test -x ${CONFIGS_PATH}/submodules/dmenu/dmenu || {
 }
 
 # fugitive vim plugin
+echo Set up fugitive vim plugin
 git -C ${CONFIGS_PATH} submodule update --init submodules/vim-fugitive
 vim -n -e --noplugin --cmd  "helptags submodules/vim-fugitive|quit"
 
+echo Build helper programs
 # c-program helpers
 (
 	make -C ${CONFIGS_PATH}/c-programs insdirs2 prefix dumptags || {
 		printf $'\n\e[1mFAILED to build helper programs for shell dmenu integration. Sorry!\n\n'
 	}
 )
+
+echo Git config
+INSTALL_GIT_INCLUDE=true
+GITCONFIG_PATH=$(readlink -m ${CONFIGS_PATH}/gitconfig)
+while read CFG_VALUE; do
+	if [ "$CFG_VALUE" = "$GITCONFIG_PATH" ]; then
+		INSTALL_GIT_INCLUDE=false
+	fi
+done <<< "$(git config --global --get-all include.path)"
+
+${INSTALL_GIT_INCLUDE} && git config --global --add include.path "$GITCONFIG_PATH"
+
