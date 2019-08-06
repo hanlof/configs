279b24643c02d2e566f068febce9007adec6bc4b
Hans Löfving <hanlof@gmail.com>
2019-08-06 17:57:44 +0200

    Install script and TODO comments update

--- install.sh
+++ install.sh
@@ -9,23 +9,24 @@
 # DONE: install gitconfig include using git config mechanism
 
 # TODO: consider global (system-wide) git options (hooks) that may interfere with this local git repo and/or other repos
-# TODO: update vim config so it can detect dmenu path, and only use -w if version is >= 4.7
-# TODO: consider adding index_repo to path and/or as an alias in configs/bashrc
-# TODO: sudo apt-get install exuberant-ctags vim vim-gtk3 git gitk gcc make. what else??
-# TODO: check repo root hash in vimrc_template. set up indentation style to comply with repo coding standards
-# TODO: consider different indentation styles in different paths within a repo?
+# TODO: sudo apt-get install vim vim-gtk3 git gitk gcc make. what else??
+# TODO: build universal-ctags
 # TODO: do some SED magic for paths and install template files. gonna need some interactivity if files exists
 # TODO: set up ~/.vimrc to properly include $CONFIGS_PATH/dotvim/vimrc
 # TODO: set up ~/.bashrc to properly include $CONFIGS_PATH/bashrc
-# TODO: maybe set up core.email and core.name for git configuration
 # TODO: maybe set up github username for configs repo in git configuration (local or global?)
 # TODO: proper information printing while running install.sh. sort out coloring properly using tput
 # TODO: consider what dependencies we have in install.sh. git?! apt-get ?! LOLz. vim... make...
-# TODO: VIM: sticky diff colors may not be nice for users who wants a different colorscheme
-# TODO: embed dmenu in terminal emulators does not seem to work, what's the problem?!
+# TODO: MAKEFLAGS -j <n> for builds in here
 
 CONFIGS_PATH=$(dirname $0)
 CONFIGS_PATH_ABS=$(readlink -m ${CONFIGS_PATH})
+
+# submodules
+git -C ${CONFIGS_PATH} submodule update --init submodules/vim-fugitive
+git -C ${CONFIGS_PATH} submodule update --init submodules/vim-bitbake
+git -C ${CONFIGS_PATH} submodule update --init submodules/universal-ctags
+git -C ${CONFIGS_PATH} submodule update --init submodules/dmenu
 
 # dmenu
 which dmenu > /dev/null || { # first try systme-wide dmenu
@@ -42,10 +43,8 @@ which dmenu > /dev/null || test -x ${CONFIGS_PATH}/submodules/dmenu/dmenu || {
   printf $'\n\e[1mFAILED to install and/or build dmenu from source. Sorry!\n\n\e[0m'
 }
 
-echo Set up vim plugins
-git -C ${CONFIGS_PATH} submodule update --init submodules/vim-fugitive
-git -C ${CONFIGS_PATH} submodule update --init submodules/vim-bitbake
 
+echo Set up vim plugins
 # fugitive documentation
 vim -n -e --noplugin --cmd  'helptags submodules/vim-fugitive/doc|quit'
 
@@ -67,6 +66,9 @@ while read CFG_VALUE; do
 done <<< "$(git config --global --get-all include.path)"
 
 ${INSTALL_GIT_INCLUDE} && git config --global --add include.path "$GITCONFIG_PATH"
+
+echo vimrc
+test -e ~/.vimrc || cp ${CONFIGS_PATH_ABS}/vimrc_template ~/.vimrc
 
 echo Xresources
 test -e ~/.Xresources || ln -s ${CONFIGS_PATH_ABS}/Xresources ~/.Xresources
