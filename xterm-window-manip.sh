4c1eb7c7821b3ba10a77061305ef846e39583cbd
Hans Löfving <hanlof@gmail.com>
2019-08-25 11:26:59 +0200

    Interactive resize / reposition xterm window

--- xterm-window-manip.sh
+++ xterm-window-manip.sh
@@ -27,19 +27,50 @@ _get_workarea() {
 	declare -g x y w h
 }
 
-# TODO: interactive mode! move window using kjhl or arrow keys!
 # TODO: replace wl, wr commands with awesome generic window manipulation function
 #       Example: window_manip 2/1 3/2 to partition the screen in 2 equal parts for X
 #                and 3 equal parts for y, and use first slot for x and second slot for y
 # TODO: put in in-path/ ?
+res() {
+	_get_workarea
+	echo -n $'\e['18t
+	IFS=";" read -t 0.1 -d t -s _ h w _
+	mode=SIZE
+	while true; do
+		case $mode in
+			SIZE) let _x=w; let _y=h ;;
+			POS) let _x=x; let _y=y ;;
+		esac
+		echo -n $'\x0d\e[2K'$mode $_x, $_y
+		read -n 1 -s i
+		case $i in
+			j) let _y+=1;;
+			J) let _y+=5;;
+			k) let _y-=1;;
+			K) let _y-=5;;
+			l) let _x+=1;;
+			L) let _x+=5;;
+			h) let _x-=1;;
+			H) let _x-=5;;
+			o|O) case $mode in POS) mode=SIZE;; SIZE) mode=POS ;; esac; continue ;;
+			x|X|q|Q) break;;
+		esac
+		case $mode in
+			SIZE) let h=_y w=_x; echo -ne "\e[8;${h};${w}t" ;;
+			POS) let x=_x y=_y; echo -ne "\e[3;${x};${y}t"
+		esac
+	done
+	echo " Bye!"
+}
+
 wl() {
 	_get_workarea
 	echo $x $y $w $h
 	let neww=w/2
 	echo $newx $y $neww $h
-	# set new pos
-	echo -ne "\e[4;${h};${neww}t"
 	# set new size
+	echo -ne "\e[4;${h};${neww}t"
+	# set new pos
 	echo -ne "\e[3;${x};${y}t"
 }
 
