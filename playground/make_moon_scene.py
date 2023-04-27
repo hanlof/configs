4bbafeb214b5e8b3030d4380524047cba6e77425
Hans Löfving <hans.lofving@sigma.se>
2023-04-27 20:58:34 +0200

    Half decent moon scene creation script
    having a script generate the scene saves hundreds of kilobytes compared
    to including a .blend in the git repo. height/colormap not included!
    get them from https://svs.gsfc.nasa.gov/4720 and pass them as paremeters
    when launching the script

--- playground/make_moon_scene.py
+++ playground/make_moon_scene.py
@@ -1,26 +1,70 @@
 import bpy
 #import bpy.context
-from math import pi, tau
+from math import pi, tau, radians
 from mathutils import Vector
 import bmesh
 
+import sys
+import mathutils
+import astropy
+import astropy.coordinates
+from astropy.coordinates import get_sun, get_moon, EarthLocation, Longitude, Latitude, GCRS
+import astropy.units as u
+import datetime
+import lunarsky
+from lunarsky import MoonLocation
+
+
 #bpy.ops.object.mode_set(mode='OBJECT')
-transform_opts = {
-    'orient_type': 'GLOBAL',
-    'orient_matrix': ((1, 0, 0), (0, 1, 0), (0, 0, 1)),
-    'orient_matrix_type': 'GLOBAL',
-    'constraint_axis': (True, True, True),
-    'mirror': False,
-    'use_proportional_edit': False,
-    'proportional_edit_falloff': 'SMOOTH',
-    'proportional_size': 1,
-    'use_proportional_connected': False,
-    'use_proportional_projected': False }
+#transform_opts = {
+#    'orient_type': 'GLOBAL',
+#    'orient_matrix': ((1, 0, 0), (0, 1, 0), (0, 0, 1)),
+#    'orient_matrix_type': 'GLOBAL',
+#    'constraint_axis': (True, True, True),
+#    'mirror': False,
+#    'use_proportional_edit': False,
+#    'proportional_edit_falloff': 'SMOOTH',
+#    'proportional_size': 1,
+#    'use_proportional_connected': False,
+#    'use_proportional_projected': False }
+
+# worlds cameras meshes objects lights images textures materials
+def clear_scene():
+    for w in bpy.data.worlds:
+        bpy.data.worlds.remove(w, do_unlink=True)
+    for w in bpy.data.cameras:
+        bpy.data.cameras.remove(w, do_unlink=True)
+    for w in bpy.data.meshes:
+        bpy.data.meshes.remove(w, do_unlink=True)
+    for w in bpy.data.objects:
+        bpy.data.objects.remove(w, do_unlink=True)
+    for w in bpy.data.lights:
+        bpy.data.lights.remove(w, do_unlink=True)
+    for w in bpy.data.images:
+        bpy.data.images.remove(w, do_unlink=True)
+    for w in bpy.data.textures:
+        bpy.data.textures.remove(w, do_unlink=True)
+    for w in bpy.data.materials:
+        bpy.data.materials.remove(w, do_unlink=True)
+
+def clear_nodes(nodes):
+    for n in nodes:
+        nodes.remove(n)
+
+def make_camera(name, object_props=None, **kwargs):
+    c = bpy.data.cameras.new(name)
+    for propname, val in kwargs.items():
+        setattr(c, propname, val)
+    o = bpy.data.objects.new(name, c)
+    for propname, val in object_props.items():
+        setattr(o, propname, val)
+    bpy.context.collection.objects.link(o)
+    return o
 
 def create_plane_noops():
     mesh=bpy.data.meshes.new("Moonmesh")
     mesh.use_auto_smooth = True
-    mesh.from_pydata( 
+    mesh.from_pydata(
         (( -2, 0, -1), (2, 0, -1), (2, 0, 1), (-2, 0, 1),
          (  0, 0, -1), (0, 0, 1)),
         (),
@@ -28,6 +72,18 @@ def create_plane_noops():
     o = bpy.data.objects.new("Moon", mesh)
     bpy.context.collection.objects.link(o)
     return o
+
+def make_uv_from_faces(obj):
+    bm = bmesh.new()
+    bm.from_mesh(obj.data)
+    uv_layer = bm.loops.layers.uv.verify()
+    uv_scale  = Vector( (0.25, 0.5) )
+    uv_offset = Vector( (0.5, 0.5) )
+    for face in bm.faces:
+        face.smooth = True
+        for loop in face.loops:
+            loop[uv_layer].uv = loop.vert.co.xz * uv_scale + uv_offset
+    bm.to_mesh(obj.data)
 
 def add_mod(obj, name, type, opts):
     mod = obj.modifiers.new(name, type)
@@ -46,103 +102,241 @@ def get_center(obj):
     for v in obj.data.vertices:
         sum += v.co
     return sum / len(obj.data.vertices)
-     
 
+# clear and unlink stuff from bpy.data
+def clear_data(*args):
+    for field in [getattr(bpy.data, name) for name in args]:
+        for w in field:
+            field.remove(w, do_unlink=True)
+
+
+# Random notes...
+# bpy.data.window_managers[0].windows[0].scene['a'] = 1
+
+# bpy.data.window_managers[0].windows[0].scene.collection.all_objects[:]
+# bpy.context.window.scene=bpy.data.scenes[1]
+
+# bpy.data.window_managers[0].windows[0].scene
+# bpy.data.scenes['What']
+# bpy.data.window_managers[0].windows[0].screen
+# bpy.data.screens['Scripting']
+
+# Constants    
 moon_radius=1.7371
 moon_diameter = 2 * moon_radius
 
-for w in bpy.data.worlds:
-    bpy.data.worlds.remove(w, do_unlink=True)
-for w in bpy.data.cameras:
-    bpy.data.cameras.remove(w, do_unlink=True)
-for w in bpy.data.meshes:
-    bpy.data.meshes.remove(w, do_unlink=True)
-for w in bpy.data.objects:
-    bpy.data.objects.remove(w, do_unlink=True)
-for w in bpy.data.lights:
-    bpy.data.lights.remove(w, do_unlink=True)
-for w in bpy.data.images:
-    bpy.data.images.remove(w, do_unlink=True)
-for w in bpy.data.textures:
-    bpy.data.textures.remove(w, do_unlink=True)
-for w in bpy.data.materials:
-    bpy.data.materials.remove(w, do_unlink=True)
 
+# TODO: pick one of these ways to clear the scene
+#clear_data('worlds', 'cameras', 'meshes', 'objects', 'lights', 'images', 'textures', 'materials')
+#clear_scene()
 
-# Origin 
-origin_obj = bpy.data.objects.new("Origin", None)
-bpy.context.collection.objects.link(origin_obj)
+moonscene = bpy.context.scene
 
-# Camera    
-c=bpy.data.cameras.new("Earth Viewpoint")
-o=bpy.data.objects.new("Earth Viewpoint", c)
-o.location = Vector( (0, -10, 0) )
-bpy.context.scene.camera = o
-bpy.context.collection.objects.link(o)
+default_props = {
+    'camfov': 0.6,
+    'resx': 1024,
+    'resy': 1024,
+    'colormap': '/home/hlofving/Downloads/MoonColorMap.png',
+    'heightmap': '/home/hlofving/Downloads/MoonReliefMap.png', }
+    
 
+moonprops = default_props
 
+if 'moon_params' in moonscene:
+    print("found moon_props", moonscene['moon_params'])
+    moonprops.update(moonscene['moon_params'])
+
+print("fov", moonprops['camfov'])
+
+moonscene.render.film_transparent = True
+moonscene.render.resolution_x = moonprops['resx']
+moonscene.render.resolution_y = moonprops['resy']
+
+for o in moonscene.collection.all_objects:
+    bpy.data.objects.remove(o)
+
+# Camera
+cam = make_camera("Earth Viewpoint", lens_unit='FOV', angle=radians(moonprops['camfov']), clip_end=500,
+                      object_props={'location': Vector( (0, 0, 0) )})
+moonscene.camera = cam
+
+# Origin (center of earth)
+origin_obj = bpy.data.objects.new("Origin", None) # our bending point
+moonscene.collection.objects.link(origin_obj)
+
+origin_obj = bpy.data.objects.new("Observer Location", None) # our bending point
+moonscene.collection.objects.link(origin_obj)
+
+origin_obj = bpy.data.objects.new("Observer Down Point", None) # our bending point
+moonscene.collection.objects.link(origin_obj)
+
+origin_obj = bpy.data.objects.new("Moon North", None) # our bending point
+moonscene.collection.objects.link(origin_obj)
+
+origin_obj = bpy.data.objects.new("Moon East", None) # our bending point
+moonscene.collection.objects.link(origin_obj)
+
+# Moon object
 moon_obj = create_plane_noops()
 moon_obj.data.use_auto_smooth = True
-
-# create the UV map (have to use bmesh i guess...)
-bm = bmesh.new()
-bm.from_mesh(moon_obj.data)
-uv_layer = bm.loops.layers.uv.verify()
-uv_scale  = Vector( (0.25, 0.5) )
-uv_offset = Vector( (0.5, 0.5) )
-for face in bm.faces:
-    face.smooth = True
-    for loop in face.loops:
-        loop[uv_layer].uv = loop.vert.co.xz * uv_scale + uv_offset
-bm.to_mesh(moon_obj.data)
-
-add_mod(moon_obj, "Subsurf", "SUBSURF", \
+make_uv_from_faces(moon_obj)
+add_mod(moon_obj, "Subsurf", "SUBSURF",
     { 'subdivision_type': 'SIMPLE', 'levels': 6, 'render_levels': 6 } )
 add_mod(moon_obj, "Bend X", "SIMPLE_DEFORM",
-    { 'deform_axis': 'X', 'deform_method': 'BEND',
-      'origin': bpy.data.objects['Origin'], 'angle': pi } )
+    { 'deform_axis': 'X', 'deform_method': 'BEND', 'origin': origin_obj, 'angle': pi } )
 add_mod(moon_obj, "Bend Z", "SIMPLE_DEFORM",
-    { 'deform_axis': 'Z', 'deform_method': 'BEND',
-      'origin': bpy.data.objects['Origin'], 'angle': tau } )
+    { 'deform_axis': 'Z', 'deform_method': 'BEND', 'origin': origin_obj, 'angle': tau } )
 apply_mods(moon_obj)
-
-# translate the verticies so the origin is in the middle of the sphere
-offset = get_center(moon_obj)
+offset = get_center(moon_obj) # make origin be in the center and set the distances precisely
 for v in moon_obj.data.vertices:
     v.co -= offset
     v.co.length = moon_radius
-    
+
+
+"""
+
+Node('ShaderNodeMatOutput',
+    attrs = {'what': 'ok', 'etc': 'osv' },
+    inputs = {
+        'SlotName': Node('SomethingElse',
+            attrs { 1: 2 }
+        )
+    }
+)
+
+new_node(ntree, "Name", "type"
+        input_defs={'A': 1, 'B': 2},
+        attrs={'what': 'ok'})
+
+make_link("name", "slotname", "name", "slotname")
+
+"""
+
 # Material
-mat = bpy.data.materials.new(name='Moon Surface')
-mat.use_nodes=True
-moon_obj.data.materials.append(mat)
+moonmat = bpy.data.materials.new(name = 'Moon Surface')
+moon_obj.data.materials.append(moonmat)
 
-mat_nodes = mat.node_tree.nodes
+# This is a magic operator which creates a node tree with a principled BSDF...
+moonmat.use_nodes = True
+mat_nodes = moonmat.node_tree.nodes
+clear_nodes(mat_nodes)
 
-bsd=mat_nodes["Principled BSDF"]
-bsd.inputs["Specular"].default_value = 1
-bsd.inputs["Roughness"].default_value = 1
+mat_nodes.new("ShaderNodeOutputMaterial").name = "Material Output"
+mat_nodes["Material Output"].is_active_output = True
+
+mat_nodes.new("ShaderNodeBsdfPrincipled").name = "Principled BSDF"
+mat_nodes["Principled BSDF"].inputs["Specular"].default_value = 1
+mat_nodes["Principled BSDF"].inputs["Roughness"].default_value = 1
+
+mat_nodes.new("ShaderNodeTexImage").name = "Image Texture Color"
+mat_nodes['Image Texture Color'].location = mat_nodes["Principled BSDF"].location + Vector ( ( -300, 0 ) )
+mat_nodes['Image Texture Color'].image = bpy.data.images.load(moonprops['colormap'])
+mat_nodes['Image Texture Color'].label = "Moon Color Image"
+
+mat_nodes.new("ShaderNodeTexImage").name = 'Image Texture Heightmap'
+mat_nodes['Image Texture Heightmap'].location = mat_nodes["Principled BSDF"].location + Vector ( ( -600, -300 ) )
+mat_nodes['Image Texture Heightmap'].image = bpy.data.images.load(moonprops['heightmap'])
+mat_nodes['Image Texture Heightmap'].label = "Moon Height Map"
+
+mat_nodes.new("ShaderNodeVectorDisplacement").name = 'Vector Displacement'
+mat_nodes['Vector Displacement'].location = mat_nodes["Principled BSDF"].location + Vector ( ( -300, -300 ) )
+mat_nodes['Vector Displacement'].inputs["Scale"].default_value = 0.02
+
+moonmat.node_tree.links.new(mat_nodes['Image Texture Color'].outputs["Color"], mat_nodes["Principled BSDF"].inputs['Base Color'])
+moonmat.node_tree.links.new(mat_nodes['Vector Displacement'].outputs["Displacement"], mat_nodes["Material Output"].inputs['Displacement'])
+moonmat.node_tree.links.new(mat_nodes['Image Texture Heightmap'].outputs["Color"], mat_nodes['Vector Displacement'].inputs['Vector'])
+moonmat.node_tree.links.new(mat_nodes['Principled BSDF'].outputs["BSDF"], mat_nodes["Material Output"].inputs['Surface'])
+
+# Light
+moonscene.collection.objects.link(bpy.data.objects.new("Sun", bpy.data.lights.new("Sun", "SUN")))
+bpy.data.lights["Sun"].energy = 8.4
+bpy.data.lights["Sun"].angle = 0
 
 
-colors_input_node = mat_nodes.new("ShaderNodeTexImage")
-colors_input_node.location = bsd.location + Vector ( ( -300, 0 ) )
-colors_input_node.image=bpy.data.images.load("/home/hlofving/Downloads/MoonColorMap.png")
+# updating positions!
 
-height_input_node=mat_nodes.new("ShaderNodeTexImage")
-height_input_node.location = bsd.location + Vector ( ( -600, -300 ) )
-height_input_node.image=bpy.data.images.load("/home/hlofving/Downloads/MoonReliefMap.png")
+now = astropy.time.Time(datetime.datetime.utcnow())
 
-vector_disp_node=mat_nodes.new("ShaderNodeVectorDisplacement")
-vector_disp_node.location = bsd.location + Vector ( ( -300, -300 ) )
-vector_disp_node.inputs["Scale"].default_value = 0.02
+# find coordinates for sun, moon, gbg
+sunvec = Vector(get_sun(now).cartesian.xyz.to(u.au).value)
+moonvec = Vector(get_moon(now).cartesian.xyz.to(u.Mm).value)
 
-mat.node_tree.links.new(colors_input_node.outputs["Color"], bsd.inputs['Base Color'])
-mat.node_tree.links.new(vector_disp_node.outputs["Displacement"], mat_nodes["Material Output"].inputs['Displacement'])
-mat.node_tree.links.new(height_input_node.outputs["Color"], vector_disp_node.inputs['Vector'])
+gothenburg_el = EarthLocation(lat=57.71788*u.deg, lon=11.93394*u.deg, height=80*u.m)
+observer_location=gothenburg_el
+observervec = Vector(observer_location.get_gcrs(now).cartesian.without_differentials().xyz.to(u.Mm).value)
+observer_down_vec=Vector( (0, 0, 0) )
 
-bpy.ops.object.light_add(type='SUN', radius=1, align='WORLD', location=(0, -10, 0), scale=(1, 1, 1))
-bpy.context.object.data.energy = 7
-bpy.context.object.data.angle = 0
+# Update placeholders for moons position
+moon_north_point = MoonLocation(Longitude(0*u.deg), Latitude(90*u.deg))
+moon_north_gcrs = moon_north_point.get_mcmf(obstime=now).transform_to(GCRS(obstime=now))
+moon_north_vec = Vector(moon_north_gcrs.cartesian.without_differentials().xyz.to(u.Mm).value)
+moon_east_point = MoonLocation(Longitude(90*u.deg), Latitude(0*u.deg))
+moon_east_gcrs = moon_east_point.get_mcmf(obstime=now).transform_to(GCRS(obstime=now))
+moon_east_vec = Vector(moon_east_gcrs.cartesian.without_differentials().xyz.to(u.Mm).value)
 
-bpy.context.scene.render.film_transparent = True
+print("Sun", sunvec, sunvec.length)
+print("Moon", moonvec, moonvec.length)
+print("Observer", observervec, observervec.length)
+print("Moon North", moon_north_vec, moon_north_vec.length)
+print("Moon East", moon_east_vec, moon_east_vec.length)
 
+bpy.data.objects["Sun"].location = moonvec + (sunvec * 10)
+bpy.data.objects['Moon'].location = moonvec
+bpy.data.objects["Moon North"].location = moon_north_vec
+bpy.data.objects["Moon East"].location = moon_east_vec
+bpy.data.objects["Observer Location"].location = observervec
+bpy.data.objects["Observer Down Point"].location = observer_down_vec
+
+# make sun track the moon
+c = bpy.data.objects['Sun'].constraints.new("TRACK_TO")
+c.target = bpy.data.objects['Moon']
+
+# align a helper axis from earth center to the observer
+c = bpy.data.objects['Observer Down Point'].constraints.new("TRACK_TO")
+c.target = bpy.data.objects["Observer Location"]
+c.up_axis = 'UP_X'
+c.track_axis = 'TRACK_Y'
+
+# make the camera track the moon using "UP" as pointing away from earth center!
+c = cam.constraints.new("COPY_LOCATION")
+c.target = bpy.data.objects['Observer Location']
+
+c = cam.constraints.new("TRACK_TO")
+c.target = bpy.data.objects['Observer Down Point']
+c.track_axis = 'TRACK_NEGATIVE_Y'
+c.up_axis = 'UP_X'
+c = cam.constraints.new("LOCKED_TRACK")
+c.target = bpy.data.objects['Moon']
+c.track_axis = 'TRACK_NEGATIVE_Z'
+c.lock_axis = 'LOCK_Y'
+c = cam.constraints.new("LOCKED_TRACK")
+c.target = bpy.data.objects['Moon']
+c.track_axis = 'TRACK_NEGATIVE_Z'
+c.lock_axis = 'LOCK_X'
+
+c = moon_obj.constraints.new("LOCKED_TRACK")
+c.target = bpy.data.objects['Moon North']
+c.track_axis = 'TRACK_Z'
+c.lock_axis = 'LOCK_X'
+
+c = moon_obj.constraints.new("LOCKED_TRACK")
+c.target = bpy.data.objects['Moon East']
+c.track_axis = 'TRACK_X'
+c.lock_axis = 'LOCK_Z'
+
+
+"""
+
+# adjust the moons rotation with the help of north and east point
+bpy.ops.object.constraint_add(type='LOCKED_TRACK')
+bpy.context.object.constraints["Locked Track"].track_axis = 'TRACK_Z'
+bpy.context.object.constraints["Locked Track"].target = bpy.data.objects["Moon North"]
+bpy.context.object.constraints["Locked Track"].lock_axis = 'LOCK_Y'
+bpy.context.object.constraints["Locked Track"].lock_axis = 'LOCK_X'
+bpy.ops.object.constraint_add(type='LOCKED_TRACK')
+bpy.context.object.constraints["Locked Track.001"].target = bpy.data.objects["Moon East"]
+bpy.context.object.constraints["Locked Track.001"].track_axis = 'TRACK_X'
+bpy.context.object.constraints["Locked Track.001"].track_axis = 'TRACK_NEGATIVE_X'
+
+bpy.ops.render.render(write_still=True)
+"""
--- playground/moonoverlay.sh
+++ playground/moonoverlay.sh
@@ -11,6 +11,22 @@ python3.9 kery-svg-chart.py
 # background stars. use pngphoon with 0 moons drawn.
 BGCMD="""pngphoon -w 1920 -h 1080 -s 111 -x 0 -B '${STARBGCOLOR}' -F '${COL_ORANGE}' -f -"""
 
+# TODO: decrease size of moon, sun and chart
+# TODO: somehow use the circle sizes at the absolute measurement for the images and not the bounding boxes
+
+# TODO: Sun image is updated every ~3.9 minutes (measured in the morning of 2023-04-20)
+# TODO: check date on raw-sun image before downloading it (don't refresh too often)
+# TODO: tweak the sun so that more of the corona shows up clearly
+
+# $ curl https://services.swpc.noaa.gov/images/animations/suvi/primary/171/
+# <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+# <html>
+# [.....]
+# <a href="latest.png">latest.png</a>              2023-04-20 08:12  644K
+# <a href="or_suvi-l2-ci171_g16_s20230420T051200Z_e20230420T051600Z_v1-0-1.png">or_suvi-l2-ci171_g16..&gt;</a> 2023-04-20 05:23  646K
+# <a href="or_suvi-l2-ci171_g16_s20230420T051600Z_e20230420T052000Z_v1-0-1.png">or_suvi-l2-ci171_g16..&gt;</a> 2023-04-20 05:27  646K
+# [.....]
+
 # moon phase info box
 label="$(python3 phase3.py --phase)"
 MOONPHASECMD="""convert \
@@ -22,25 +38,32 @@ MOONPHASECMD="""convert \
 
 # call python script to get the svg path for moon shadow and overlay it onto raw moon image, plus some fiddling with colors!
 moonshadepath=$(python3 ./phase3.py --shade-svgpath ./fullmoon-template.png)
-MOONCMD="""convert /home/hlofving/configs/playground/fullmoon-template.png -channel RGB +level 20%,40% -draw 'fill #${STARBGCOLOR}f0 stroke #00000000 path \"${moonshadepath}\"' png:-"""
+MOONCMD="""convert /home/hlofving/configs/playground/fullmoon-template.png -channel RGB +level 20%,40% -draw 'fill #${STARBGCOLOR}80 stroke #00000000 path \"${moonshadepath}\"' png:-"""
 
 # fetch sun and make a nice alpha channel and tint it with foreground color
 # best awesomest: https://services.swpc.noaa.gov/images/animations/suvi/primary/171/latest.png
 SUN_RAW_FNAME='sun-raw.png'
 wget https://services.swpc.noaa.gov/images/animations/suvi/primary/171/latest.png -O ${SUN_RAW_FNAME}
 SUNCMD="""convert -crop 1280x1222+0+0 ${SUN_RAW_FNAME} -separate -average -colorspace gray -level 0,80% -alpha off -fill '#${COL_ORANGE}' -tint 100 \
-       \( -clone 0 -colorspace gray -brightness-contrast 30,30 -level 30%,60% -draw 'fill white stroke none circle 640,640 640,250' \) \
+       \( -clone 0 -colorspace gray -brightness-contrast 30,30 -level 30%,90% -draw 'fill white stroke none circle 640,640 640,250' \) \
        -alpha off -compose copyopacity -composite -brightness-contrast 15,15 png:-"""
+
+# blender --python-expr "import bpy; bpy.context.scene['moon_props'] = {'colormap': 'what', 'heightmap': 'rly?', 'camera_fov': 0.6}; exit(0)"
+#DISPLAY=:0.0 blender -b /home/hlofving/playground/mymoon.blend -o /home/hlofving/playground/moon_rendered.png -P /home/hlofving/playground/moon3d/make_scene.py
+DISPLAY=:0.0 blender --python-expr "import bpy; bpy.context.window.scene=bpy.data.scenes.new('Moon Scene'); bpy.context.window.scene['moon_params']={'camfov':0.6}" -P /home/hlofving/configs/playground/make_moon_scene.py -b -o "/home/hlofving/playground/moon_rendered_##" -f 1
 
 # compose everything together
 convert <(eval "$BGCMD") \
-    \( -gravity northeast -geometry +50+70   -background none -resize 90% now.svg \) -composite \
+    \( -gravity northeast -geometry +77+50   -background none -resize 540 now.svg \) -composite \
     \( -gravity southeast -geometry +10+160  -background none -density 150 -size 150x150 grid.svg \)  -composite \
-    \( -gravity center    -geometry +300+180 -resize 555x  <(eval "$MOONCMD") \) -composite \
-    \( -gravity northwest -geometry -50-100  -resize 1111x <(eval "$SUNCMD") \) -composite \
-    \( -gravity southeast -geometry +10+30                 <(eval "$MOONPHASECMD") \) -composite \
+    \( /home/hlofving/playground/moon_rendered_01.png -resize 640x -gravity center    -geometry +0-220   -background none \) -composite \
+    \( -gravity northwest -geometry -90-111   -resize 870 <(eval "$SUNCMD") \) -composite \
+    \( -gravity southeast -geometry +10+30              <(eval "$MOONPHASECMD") \) -composite \
     /tmp/desktop-finished.png
 
+    #\( -gravity northwest -geometry +640+0   -resize 640x -background none /home/hlofving/playground/moon_rendered.png \) -composite \
+
+#    \( -gravity center    -geometry +300+180 -resize 555x -background none -rotate 45.5 <(eval "$MOONCMD") \) -composite \
 cp /tmp/desktop-finished.png /home/hlofving/moon.png
 
 # fix tint and opacity for sun
@@ -54,7 +77,4 @@ cp /tmp/desktop-finished.png /home/hlofving/moon.png
 
 # moon original: https://theskylive.com/images/fullmoon.jpg
 # convert fullmoon.jpg -level 0,70% \( -clone 0 +level-colors "#002b3600","#b58900ff" \) \( -clone 1 -alpha off -fill white -draw 'circle 400,400 2,400' \) \( -clone 1,2 -alpha off -compose copyopacity -composite \) -delete 0,1,2 +append fullmoon-template.png
-
-# decent moon + sun (Ulrica layout)
-# convert  ~/moon.png \( -fill "#b58900" -tint 60 -fill "#b58900" -gravity center -resize 640x640 -geometry +260+150 ~/fullmoon-alpha.png \) -composite \( -gravity northwest -geometry 1200x1200-100-50 /tmp/sun-final.png \) -composite png:- | display
 
