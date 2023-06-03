2c0ee3b4f8647d2f0a0ae697b5a08222671a96c1
Hans Löfving <hans.lofving@sigma.se>
2023-06-03 11:17:30 +0200

    Moon scene has moon trajectory path added

--- playground/make_moon_scene.py
+++ playground/make_moon_scene.py
@@ -1,7 +1,6 @@
 import bpy
-#import bpy.context
 from math import pi, tau, radians
-from mathutils import Vector
+from mathutils import Vector, Matrix
 import bmesh
 
 import sys
@@ -9,24 +8,13 @@ import mathutils
 import astropy
 import astropy.coordinates
 from astropy.coordinates import get_sun, get_moon, EarthLocation, Longitude, Latitude, GCRS
+from astropy.coordinates import AltAz
 import astropy.units as u
 import datetime
 import lunarsky
 from lunarsky import MoonLocation
 
 
-#bpy.ops.object.mode_set(mode='OBJECT')
-#transform_opts = {
-#    'orient_type': 'GLOBAL',
-#    'orient_matrix': ((1, 0, 0), (0, 1, 0), (0, 0, 1)),
-#    'orient_matrix_type': 'GLOBAL',
-#    'constraint_axis': (True, True, True),
-#    'mirror': False,
-#    'use_proportional_edit': False,
-#    'proportional_edit_falloff': 'SMOOTH',
-#    'proportional_size': 1,
-#    'use_proportional_connected': False,
-#    'use_proportional_projected': False }
 
 # worlds cameras meshes objects lights images textures materials
 def clear_scene():
@@ -46,6 +34,9 @@ def clear_scene():
         bpy.data.textures.remove(w, do_unlink=True)
     for w in bpy.data.materials:
         bpy.data.materials.remove(w, do_unlink=True)
+    for w in bpy.data.curves:
+        bpy.data.curves.remove(w, do_unlink=True)
+
 
 def clear_nodes(nodes):
     for n in nodes:
@@ -109,77 +100,69 @@ def clear_data(*args):
         for w in field:
             field.remove(w, do_unlink=True)
 
-
-# Random notes...
-# bpy.data.window_managers[0].windows[0].scene['a'] = 1
-
-# bpy.data.window_managers[0].windows[0].scene.collection.all_objects[:]
-# bpy.context.window.scene=bpy.data.scenes[1]
-
-# bpy.data.window_managers[0].windows[0].scene
-# bpy.data.scenes['What']
-# bpy.data.window_managers[0].windows[0].screen
-# bpy.data.screens['Scripting']
-
-# Constants    
+# Constants
 moon_radius=1.7371
 moon_diameter = 2 * moon_radius
 
-
-
-# TODO: clear world data from scene
-# TODO: adjust sun strength
-# TODO: add earthlight
-# TODO: adjust bump map to scale correctly
-# TODO: probably use linear colorspace for bump map?!
-# TODO: get the shading/colors correct
-
-moonscene = bpy.context.scene
-
 default_props = {
-    'camfov': 0.6,
+    'camfov': 0.7,
     'resx': 1024,
     'resy': 1024,
     'sunstr': 7.5,
     'colormap': '/home/hlofving/Downloads/lroc_color_poles_4k.tif',
     'heightmap': '/home/hlofving/Downloads/ldem_16_uint.tif', }
 
-moonprops = default_props
+# TODO: clear world data from scene
+# TODO: adjust sun strength
+# TODO: add earthlight
+# TODO: switch to proper height displacement mesh
+# TODO: adjust bump map to scale the height correctly
+# TODO: probably use linear colorspace for bump map?!
+# TODO: get the shading/colors correct
 
+moonscene = bpy.context.scene
+
+moonprops = default_props
 if 'moon_params' in moonscene:
-    print("found moon_props", moonscene['moon_params'])
     moonprops.update(moonscene['moon_params'])
 
-print("fov", moonprops['camfov'])
 print(moonprops)
 
 moonscene.render.film_transparent = True
 moonscene.render.resolution_x = moonprops['resx']
 moonscene.render.resolution_y = moonprops['resy']
 
-for o in moonscene.collection.all_objects:
-    if o is not None:
-        bpy.data.objects.remove(o)
+#print("This loop crashes sometimes.")
+#for o in moonscene.collection.all_objects:
+#    if o is not None:
+#        bpy.data.objects.remove(o, do_unlink=True)
 
+clear_scene()
 # Camera
+
 cam = make_camera("Earth Viewpoint", lens_unit='FOV', angle=radians(moonprops['camfov']), clip_end=500,
                       object_props={'location': Vector( (0, 0, 0) )})
 moonscene.camera = cam
 
 # Origin (center of earth)
-origin_obj = bpy.data.objects.new("Origin", None) # our bending point
+origin_obj = bpy.data.objects.new("Origin", None)
+origin_obj.empty_display_size = 0.05
 moonscene.collection.objects.link(origin_obj)
 
-origin_obj = bpy.data.objects.new("Observer Location", None) # our bending point
+origin_obj = bpy.data.objects.new("Observer Location", None)
+origin_obj.empty_display_size = 0.05
 moonscene.collection.objects.link(origin_obj)
 
-origin_obj = bpy.data.objects.new("Observer Down Point", None) # our bending point
+origin_obj = bpy.data.objects.new("Observer Down Point", None)
+origin_obj.empty_display_size = 0.05
 moonscene.collection.objects.link(origin_obj)
 
-origin_obj = bpy.data.objects.new("Moon North", None) # our bending point
+origin_obj = bpy.data.objects.new("Moon North", None)
+origin_obj.empty_display_size = 0.05
 moonscene.collection.objects.link(origin_obj)
 
-origin_obj = bpy.data.objects.new("Moon East", None) # our bending point
+origin_obj = bpy.data.objects.new("Moon East", None)
+origin_obj.empty_display_size = 0.05
 moonscene.collection.objects.link(origin_obj)
 
 # Moon object
@@ -193,37 +176,18 @@ add_mod(moon_obj, "Bend X", "SIMPLE_DEFORM",
 add_mod(moon_obj, "Bend Z", "SIMPLE_DEFORM",
     { 'deform_axis': 'Z', 'deform_method': 'BEND', 'origin': origin_obj, 'angle': tau } )
 apply_mods(moon_obj)
-offset = get_center(moon_obj) # make origin be in the center and set the distances precisely
+offset = get_center(moon_obj) # center verticies around object origin
 for v in moon_obj.data.vertices:
     v.co -= offset
     v.co.length = moon_radius
-
-
-"""
-
-Node('ShaderNodeMatOutput',
-    attrs = {'what': 'ok', 'etc': 'osv' },
-    inputs = {
-        'SlotName': Node('SomethingElse',
-            attrs { 1: 2 }
-        )
-    }
-)
-
-new_node(ntree, "Name", "type"
-        input_defs={'A': 1, 'B': 2},
-        attrs={'what': 'ok'})
-
-make_link("name", "slotname", "name", "slotname")
-
-"""
 
 # Material
 moonmat = bpy.data.materials.new(name = 'Moon Surface')
 moon_obj.data.materials.append(moonmat)
 
-# This is a magic operator which creates a node tree with a principled BSDF...
-moonmat.use_nodes = True
+
+moonmat.use_nodes = True # This is a magic operator which creates a node tree with a principled BSDF...
+
 mat_nodes = moonmat.node_tree.nodes
 clear_nodes(mat_nodes)
 
@@ -233,7 +197,7 @@ mynodes = {
     2: mat_nodes.new("ShaderNodeTexImage"),
     3: mat_nodes.new("ShaderNodeTexImage"),
     4: mat_nodes.new("ShaderNodeBump") }
-    
+
 mynodes[0].name = "Material Output"
 mynodes[0].is_active_output = True
 
@@ -257,33 +221,6 @@ moonmat.node_tree.links.new(mynodes[4].outputs["Normal"], mynodes[1].inputs['Nor
 moonmat.node_tree.links.new(mynodes[3].outputs["Color"],  mynodes[4].inputs['Height'])
 moonmat.node_tree.links.new(mynodes[1].outputs["BSDF"],   mynodes[0].inputs['Surface'])
 
-#mat_nodes.new("ShaderNodeOutputMaterial").name = "Material Output"
-#mat_nodes["Material Output"].is_active_output = True
-
-#mat_nodes.new("ShaderNodeBsdfPrincipled").name = "Principled BSDF"
-#mat_nodes["Principled BSDF"].inputs["Specular"].default_value = 1
-#mat_nodes["Principled BSDF"].inputs["Roughness"].default_value = 1
-#mat_nodes['Principled BSDF'].location = mat_nodes["Material Output"].location + Vector ( ( -300, 0 ) )
-
-#mat_nodes.new("ShaderNodeTexImage").name = "Image Texture Color"
-#mat_nodes['Image Texture Color'].location = mat_nodes["Principled BSDF"].location + Vector ( ( -300, 0 ) )
-#mat_nodes['Image Texture Color'].image = bpy.data.images.load(moonprops['colormap'])
-#mat_nodes['Image Texture Color'].label = "Moon Color Image"
-
-#mat_nodes.new("ShaderNodeTexImage").name = 'Image Texture Heightmap'
-#mat_nodes['Image Texture Heightmap'].location = mat_nodes["Principled BSDF"].location + Vector ( ( -600, -300 ) )
-#mat_nodes['Image Texture Heightmap'].image = bpy.data.images.load(moonprops['heightmap'])
-#mat_nodes['Image Texture Heightmap'].label = "Moon Height Map"
-
-#mat_nodes.new("ShaderNodeBump").name = 'Bump'
-#mat_nodes['Bump'].location = mat_nodes["Principled BSDF"].location + Vector ( ( -300, -300 ) )
-#mat_nodes['Bump'].inputs["Strength"].default_value = 0.25
-
-#moonmat.node_tree.links.new(mat_nodes['Image Texture Color'].outputs["Color"], mat_nodes["Principled BSDF"].inputs['Base Color'])
-#moonmat.node_tree.links.new(mat_nodes['Bump'].outputs["Normal"], mat_nodes["Principled BSDF"].inputs['Normal'])
-#moonmat.node_tree.links.new(mat_nodes['Image Texture Heightmap'].outputs["Color"], mat_nodes['Bump'].inputs['Height'])
-#moonmat.node_tree.links.new(mat_nodes['Principled BSDF'].outputs["BSDF"], mat_nodes["Material Output"].inputs['Surface'])
-
 # Light
 moonscene.collection.objects.link(bpy.data.objects.new("Sun", bpy.data.lights.new("Sun", "SUN")))
 bpy.data.lights["Sun"].energy = moonprops['sunstr']
@@ -291,7 +228,6 @@ bpy.data.lights["Sun"].angle = 0
 
 
 # updating positions!
-
 now = astropy.time.Time(datetime.datetime.utcnow())
 
 # find coordinates for sun, moon, gbg
@@ -299,8 +235,7 @@ sunvec = Vector(get_sun(now).cartesian.xyz.to(u.au).value)
 moonvec = Vector(get_moon(now).cartesian.xyz.to(u.Mm).value)
 
 gothenburg_el = EarthLocation(lat=57.71788*u.deg, lon=11.93394*u.deg, height=80*u.m)
-observer_location=gothenburg_el
-observervec = Vector(observer_location.get_gcrs(now).cartesian.without_differentials().xyz.to(u.Mm).value)
+observervec = Vector(gothenburg_el.get_gcrs(now).cartesian.without_differentials().xyz.to(u.Mm).value)
 observer_down_vec=Vector( (0, 0, 0) )
 
 # Update placeholders for moons position
@@ -311,11 +246,11 @@ moon_east_point = MoonLocation(Longitude(90*u.deg), Latitude(0*u.deg))
 moon_east_gcrs = moon_east_point.get_mcmf(obstime=now).transform_to(GCRS(obstime=now))
 moon_east_vec = Vector(moon_east_gcrs.cartesian.without_differentials().xyz.to(u.Mm).value)
 
-print("Sun", sunvec, sunvec.length)
-print("Moon", moonvec, moonvec.length)
-print("Observer", observervec, observervec.length)
-print("Moon North", moon_north_vec, moon_north_vec.length)
-print("Moon East", moon_east_vec, moon_east_vec.length)
+#print("Sun", sunvec, sunvec.length)
+#print("Moon", moonvec, moonvec.length)
+#print("Observer", observervec, observervec.length)
+#print("Moon North", moon_north_vec, moon_north_vec.length)
+#print("Moon East", moon_east_vec, moon_east_vec.length)
 
 bpy.data.objects["Sun"].location = moonvec + (sunvec * 10)
 bpy.data.objects['Moon'].location = moonvec
@@ -334,7 +269,7 @@ c.target = bpy.data.objects["Observer Location"]
 c.up_axis = 'UP_X'
 c.track_axis = 'TRACK_Y'
 
-# make the camera track the moon using "UP" as pointing away from earth center!
+# make camera "up" vector align with earth center
 c = cam.constraints.new("COPY_LOCATION")
 c.target = bpy.data.objects['Observer Location']
 
@@ -351,29 +286,107 @@ c.target = bpy.data.objects['Moon']
 c.track_axis = 'TRACK_NEGATIVE_Z'
 c.lock_axis = 'LOCK_X'
 
+# adjust the moons rotation with the help of north and east point
 c = moon_obj.constraints.new("LOCKED_TRACK")
 c.target = bpy.data.objects['Moon North']
 c.track_axis = 'TRACK_Z'
 c.lock_axis = 'LOCK_X'
-
 c = moon_obj.constraints.new("LOCKED_TRACK")
 c.target = bpy.data.objects['Moon East']
 c.track_axis = 'TRACK_X'
 c.lock_axis = 'LOCK_Z'
 
 
-"""
+# Add moon trajectory path
+camdirection = moon_obj.location - observervec
+rotaxis_alt = camdirection.cross(observervec)
+rotaxis_az = Vector(observervec)
+rotaxis_az.negate() # get the correct positive rotation orientation
 
-# adjust the moons rotation with the help of north and east point
-bpy.ops.object.constraint_add(type='LOCKED_TRACK')
-bpy.context.object.constraints["Locked Track"].track_axis = 'TRACK_Z'
-bpy.context.object.constraints["Locked Track"].target = bpy.data.objects["Moon North"]
-bpy.context.object.constraints["Locked Track"].lock_axis = 'LOCK_Y'
-bpy.context.object.constraints["Locked Track"].lock_axis = 'LOCK_X'
-bpy.ops.object.constraint_add(type='LOCKED_TRACK')
-bpy.context.object.constraints["Locked Track.001"].target = bpy.data.objects["Moon East"]
-bpy.context.object.constraints["Locked Track.001"].track_axis = 'TRACK_X'
-bpy.context.object.constraints["Locked Track.001"].track_axis = 'TRACK_NEGATIVE_X'
+moon_altaz_now = get_moon(now).transform_to(AltAz(obstime=now, location=gothenburg_el))
 
-bpy.ops.render.render(write_still=True)
-"""
+POINTS = 7
+TIMESPAN = datetime.timedelta(seconds=300)
+
+# clear out the old!
+for i in bpy.data.curves:
+    bpy.data.curves.remove(i, do_unlink=True)
+
+moonscene.collection.objects.link(
+    bpy.data.objects.new("Moon Trajectory", bpy.data.curves.new('Moon Trajectory', "CURVE")))
+bpy.data.objects['Moon Trajectory'].location = moon_obj.location
+spline = bpy.data.curves['Moon Trajectory'].splines.new("BEZIER")
+spline.bezier_points.add(POINTS -1)
+
+# bpy.context.object.data.pixel_factor = 30
+
+
+gpencil_data = bpy.data.grease_pencils.new("Moon Trajectory")
+gpencil = bpy.data.objects.new(gpencil_data.name, gpencil_data)
+bpy.context.collection.objects.link(gpencil)
+gp_layer = gpencil_data.layers.new("lines")
+gp_layer.use_lights = False
+gp_layer.opacity = 0.5
+gp_frame = gp_layer.frames.new(bpy.context.scene.frame_current)
+gp_stroke = gp_frame.strokes.new()
+
+gp_stroke.line_width = 12
+gp_stroke.start_cap_mode = 'ROUND'
+gp_stroke.end_cap_mode = 'ROUND'
+gp_stroke.use_cyclic = False
+
+gp_stroke.points.add(POINTS)
+
+for i in range(POINTS):
+    time = now - (TIMESPAN / 2) + (TIMESPAN / POINTS) * (i + 0.5)
+
+    moonaltaz = get_moon(time).transform_to(AltAz(obstime=time, location=gothenburg_el))
+    alt_delta = moonaltaz.alt - moon_altaz_now.alt
+    az_delta = moonaltaz.az - moon_altaz_now.az
+    dist_delta = moonaltaz.distance - moon_altaz_now.distance
+    dist_delta = dist_delta.to(u.Mm).value
+    print(az_delta.degree, alt_delta.degree, moonaltaz.distance.to(u.km))
+
+    pathobj = bpy.data.objects.new("Moon Path " + str(i), None)
+
+#    moonscene.collection.objects.link(pathobj)
+
+    M = (
+#        Matrix.Diagonal( (1,) * 3).to_4x4() @ # Scaling can be fun?!
+        Matrix.Translation(observervec) @
+        Matrix.Rotation(alt_delta.rad, 4, rotaxis_alt) @
+        Matrix.Rotation(az_delta.rad, 4, rotaxis_az) @
+        Matrix.Translation(moon_obj.location - observervec) )
+
+    pathobj.matrix_world = M @ pathobj.matrix_world
+#    spline.bezier_points[i].co = pathobj.location - moon_obj.location
+    spline.bezier_points[i].handle_left_type = 'AUTO'
+    spline.bezier_points[i].handle_right_type = 'AUTO'
+    
+    gp_stroke.points[i].co = pathobj.location - moon_obj.location
+    gp_stroke.points[i].pressure = 1
+    gp_stroke.points[i].vertex_color = (0.0193818, 0.258183, 0.64448, 0.5)
+
+
+gpencil.location =  moon_obj.location
+#pts = [(0.0, 0.0, -1.0), (0.0, 0.0, 1.0), (-1.0, 0.0, -0.5), (0.5, 0.0, -0.5)]
+#for item, value in enumerate(pts):
+#    gp_stroke.points[item].co = value
+
+mat = bpy.data.materials.new(name="Black")
+bpy.data.materials.create_gpencil_data(mat)
+gpencil.data.materials.append(mat)
+
+mat.grease_pencil.show_fill = False
+mat.grease_pencil.fill_color = (1.0, 0.0, 1.0, 1.0)
+mat.grease_pencil.color = (0.0193818, 0.258183, 0.64448, 1) # solarized blue
+
+bpy.context.scene.view_layers["ViewLayer"].use_pass_z = True # needed for grease pencil
+
+def jdn():
+    return astropy.time.Time(datetime.datetime.now()).jd
+
+moonscene['jdn'] = astropy.time.Time(datetime.datetime.now()).jd
+what=moonscene.id_properties_ui('jdn')
+what.update(min=0, step=1)
+bpy.app.driver_namespace['jdn'] = jdn
