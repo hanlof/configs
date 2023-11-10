f9f8e5b1f08c8133f10f7ede113361737834c79d
Hans Löfving <hans.lofving@sigma.se>
2023-11-10 23:19:03 +0100

    tabbed adjustments plus random stuff

--- xterm-event-handler.sh
+++ xterm-event-handler.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-date > /tmp/log
+date >> /tmp/xterm-event-handler-log
+echo $1 >> /tmp/xterm-event-handler-log
 
 read child1 _ < /proc/${PPID}/task/${PPID}/children
-
+read _ _ _ parentpid _ < /proc/${PPID}/stat
 OUTPUT=/proc/${child1}/fd/0
 
-exec > ${OUTPUT}
+read parentcmd _ < /proc/${parentpid}/comm
+echo -n parent: $parentpid $parentcmd >> /tmp/xterm-event-handler-log
 
-source /home/hlofving/configs/bash/xterm-functions.sh
+test "$parentcmd" = "tabbed" && exit
 
-rand_xterm_geometry <> ${OUTPUT}
+case $1 in
+    Map)
+        exec > ${OUTPUT}
+        source /home/hlofving/configs/bash/xterm-functions.sh
+        rand_xterm_geometry <> ${OUTPUT}
+        ;;
+    Prop)
+        echo prop >> /tmp/xterm-event-handler-timestamp
+        #echo property event handler > ${OUTPUT}
+        #echo -n '<P>' > ${OUTPUT}
+        ;;
+esac
+date >> /tmp/xterm-event-handler-log
