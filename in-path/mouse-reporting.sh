e9d65c60f7a06f2d20bc550bec4e455ba39d5c01
Hans Löfving <hans.lofving@sigma.se>
2024-02-29 09:14:38 +0100

    in-path stuff

--- in-path/mouse-reporting.sh
+++ in-path/mouse-reporting.sh
@@ -3,10 +3,11 @@
 trap "{ echo -e '\e[?1000l' ; }" EXIT
 echo -ne "\e[?1000h"
 
-LINES="""\
-AAAA
-BBBBB
+COMMANDS="""\
+ls -l
+echo hej
 DDDDDDD
+xxxxxxxxxxxxxxxxxxxx
 EEEE
 CCC\
 """
@@ -14,6 +15,7 @@ CCC\
 get_cursor() {
 	echo -ne '\e[6n'
 	read -s -n 99 -t 0.01 POS
+	# aoeuntheaou45;3
 	[[ "$POS" =~ ([[:digit:]]+)\;([[:digit:]]+) ]] && {
 		CURSOR_Y="${BASH_REMATCH[1]}"
 		CURSOR_X="${BASH_REMATCH[2]}"
@@ -31,8 +33,8 @@ draw_menu() {
 		if [ $CNT -ne 0 ]; then echo; fi
 		LINES_ARRAY+=("$LINE")
 		let CNT+=1
-		echo -n $CNT $LINE
-	done <<< "${LINES}"
+		echo -n $CNT "$LINE"
+	done <<< "${COMMANDS}"
 	get_cursor
 	STARTING_LINE=$((CURSOR_Y - CNT))
 }
@@ -47,25 +49,17 @@ while true; do
 			q) break ;;
 		esac
 	fi
-        #echo "got <esc>"
 
         read -s -t 0.01 -n 1 _KEY
         if [ "$_KEY" != $'[' ]; then continue; fi
-        #echo "got ["
-
-	# continue
 
         read -s -n 1 _KEY
         if [ "$_KEY" != $'M' ]; then continue; fi
-        #echo "got M"
-
 
 	WHAT=""
 	while read -s -n 1 -t 0.01 _X; do
 		WHAT+="$_X"
-		#echo got $_
 	done
-	#echo -n "${WHAT}" | xxd
 
 	if [ ${#WHAT} == "2" ]; then
 		PRESS=1
@@ -80,7 +74,8 @@ while true; do
 	let Y-=33
 	LINE_INDEX=$((Y-STARTING_LINE))
 	if [ $PRESS -eq 1 -a $LINE_INDEX -ge 0 -a $LINE_INDEX -le $CNT ]; then
-		echo -e '\n>>>' $LINE_INDEX ${LINES_ARRAY[$LINE_INDEX]}
+		echo -e '\n>>>' "${LINES_ARRAY[$LINE_INDEX]}"
+		eval "${LINES_ARRAY[$LINE_INDEX]}"
 		draw_menu
 	fi
 done
---
+++ in-path/open_vim_session.sh
@@ -0,0 +1,3 @@
+#!/bin/dash
+
+exec xterm -e vim -S "${@}"
