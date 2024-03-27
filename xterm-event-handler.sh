10790fdf4e9163ad151c15092c0a4a740ee0031c
Hans Löfving <hans@haleytek.com>
2024-03-27 15:05:51 +0100

    xterm event handler tweak

--- xterm-event-handler.sh
+++ xterm-event-handler.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-date >> /tmp/xterm-event-handler-log
 echo $1 >> /tmp/xterm-event-handler-log
+date >> /tmp/xterm-event-handler-log
 
 read child1 _ < /proc/${PPID}/task/${PPID}/children
 OUTPUT=/proc/${child1}/fd/0
+read childcmd _ < /proc/${child1}/comm
 
 read _ _ _ parentpid _ < /proc/${PPID}/stat
 read parentcmd _ < /proc/${parentpid}/comm
-echo -n parent: $parentpid $parentcmd >> /tmp/xterm-event-handler-log
+echo parent: $parentpid $parentcmd - child: ${childcmd} >> /tmp/xterm-event-handler-log
 
 test "$parentcmd" = "tabbed" && exit
 
 case $1 in
     Map)
         exec > ${OUTPUT}
-        source /home/hlofving/configs/bash/xterm-functions.sh
+        source /home/hans/configs/bash/xterm-functions.sh
         rand_xterm_geometry <> ${OUTPUT}
         ;;
     Prop)
@@ -24,4 +25,3 @@ case $1 in
         #echo -n '<P>' > ${OUTPUT}
         ;;
 esac
-date >> /tmp/xterm-event-handler-log
